e3b31226a80b5f4f6357f25ed8f29025e709d5c2
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-04-19 20:32:11 -0400

    Smaller filesize

--- annoyance.py
+++ annoyance.py
@@ -1,13 +1,3 @@
-from tkinter import *
-import webbrowser
-a = Tk()
-Label(a, text="HI").pack()
-hi = Toplevel
+from webbrowser import open
 while True:
-    hello = hi()
-    df = hi()
-    fdhoifsdofu = hi()
-    fsdiofsudoiuofisdufoiuew89r = hi()
-    for i in range(0, 1000):
-        webbrowser.open("https://hello.ca")
-    a.mainloop()
+    open("https://hello.ca")
