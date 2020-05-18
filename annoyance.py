18dab0bb5d64157305fdd43f9107d12c47d9909b
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-05-18 09:51:23 -0400

    Update annoyance.py

--- annoyance.py
+++ annoyance.py
@@ -1,3 +1,3 @@
 from webbrowser import open #get the open() function from the webbrowser module
-while True: #infinite loop until a `break' statement is run
+while True: #infinite loop until a `break' statement is called
     open("https://hello.ca") #open the website `hello.ca' in your browser
